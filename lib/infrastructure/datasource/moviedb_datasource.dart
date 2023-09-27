3c5ccf972a6367344adccae73b343f80ac260f3d
David gp <juliograjalesparedes127@gmail.com>
2023-09-27 19:55:25 +0200

    impementacion de models y mappers dentro de infrastructure para el datasource moviedb

---
+++ lib/infrastructure/datasource/moviedb_datasource.dart
@@ -0,0 +1,29 @@
+
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domian/datasources/movies_datasources.dart';
+import 'package:dio/dio.dart';
+
+import 'package:cinemapedia/domian/entities/movie.dart';
+
+
+class MoviesdbDatasources extends MoviesDataSources{
+
+  final dio = Dio(BaseOptions(
+    baseUrl: 'https://api.themoviedb.org/3',
+    queryParameters: {
+      'api_key' : Environment.theMovieDbKey,
+      'language' : 'es-ES'
+    }
+  ));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+
+    final response = dio.get('/movie/now_playing');
+
+    final List<Movie> movies = [];
+    
+    return movies;
+  }
+
+}
