57daa87134a14446cde34957ca84e8a4855b3c96
David Paredes <juliograjalesparedes127@gmail.com>
2023-10-05 00:51:30 +0200

    paquete intl  añadido , para el formato de numeros

---
+++ lib/config/helpers/human_formats.dart
@@ -0,0 +1,11 @@
+import 'package:intl/intl.dart';
+
+class HumanFormats {
+  static String humanReadbledNumber(double number) {
+
+    final formatterNumber =
+        NumberFormat.compactCurrency(decimalDigits: 0, symbol: '' , locale: 'en' ).format(number);
+
+    return formatterNumber;
+  }
+}
