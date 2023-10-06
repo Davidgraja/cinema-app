2f7fee04984840e047c1f0c8a68671e0315638f9
David Paredes <juliograjalesparedes127@gmail.com>
2023-10-06 19:19:14 +0200

    creacion de provider para saber si todos los provider tienen la información

---
+++ lib/presentation/providers/movies/first_loading_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'movies_providers.dart';
+
+final firstLoadingProvider = Provider<bool>((ref) {
+  final provider1 = ref.watch(nowPlayingMoviesProvider).isEmpty;
+  final provider2 = ref.watch(popularMoviesProvider).isEmpty;
+  final provider3 = ref.watch(topRatedMoviesProvider).isEmpty;
+  final provider4 = ref.watch(upcomingMoviesProvider).isEmpty;
+
+  if(provider1 || provider2 || provider3 || provider4) return true;
+
+
+  return false;
+});
