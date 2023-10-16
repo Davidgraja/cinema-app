045c8f62fb1937d4d1a7347245027413e0866980
David Paredes <juliograjalesparedes127@gmail.com>
2023-10-16 21:58:11 +0200

    referencia a movieRepositoryProvider y implementacion del delegate para showSearch

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,34 +1,52 @@
 import 'package:flutter/material.dart';
-class CustomAppBar extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+
+class CustomAppBar extends ConsumerWidget {
   const CustomAppBar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context , WidgetRef ref) {
+    final colors = Theme.of(context).colorScheme;
+    final titleStyle = Theme.of(context).textTheme.titleMedium;
 
-    final colors =Theme.of(context).colorScheme;
-    final titleStyle =Theme.of(context).textTheme.titleMedium;
+    return SafeArea(
+        bottom: false,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: Row(
+              children: [
+                Icon(
+                  Icons.movie_creation_outlined,
+                  color: colors.primary,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Cinemapedia',
+                  style: titleStyle,
+                ),
 
-    return  SafeArea(
-      bottom: false,
-      child:  Padding(
-        padding: const  EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-        child: SizedBox(
-          // color: Colors.red,
-          width: double.infinity,
-          child: Row(
-            children: [
-              Icon(Icons.movie_creation_outlined , color: colors.primary,),
-              const SizedBox(width: 10,),
-              Text('Cinemapedia' , style: titleStyle,),
-            
-            //?  Con este widget le decimos que tome do el espacio disponible 
-              const Spacer(),
-              IconButton(onPressed: (){}, icon: const Icon(Icons.search_outlined))
-      
-            ],
+                //?  Con este widget le decimos que tome do el espacio disponible
+                const Spacer(),
+                IconButton(
+                    onPressed: () {
+                      
+                      final movieRepository= ref.read(movieRepositoryProvider);
+                      
+                      showSearch(
+                        context: context, 
+                        delegate: SearchMovieDelegate(searchMovies: movieRepository.searchMovies)
+                      );
+                    },
+                    icon: const Icon(Icons.search_outlined))
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
