2a185feea05e3bfc7eb33fd208e6790315bf1cab
David Paredes <juliograjalesparedes127@gmail.com>
2023-10-17 19:56:06 +0200

    mejora de codigo

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domian/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -43,16 +44,73 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
       builder: (context, snapshot) {
         final movies = snapshot.data ?? [];
         return ListView.builder(
-          itemCount: movies.length,
-          itemBuilder: (context, index) {
-            final movie = movies[index];
-
-            return ListTile(
-              title: Text(movie.title),
-            );
-          },
-        );
+            itemCount: movies.length,
+            itemBuilder: (context, index) =>
+                _MovieItemSearch(movie: movies[index]));
       },
+    );
+  }
+}
+
+class _MovieItemSearch extends StatelessWidget {
+  final Movie movie;
+  const _MovieItemSearch({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10 , vertical: 5),
+      child:  Row(
+        children: [
+
+         // Image
+
+          SizedBox(
+            width: size.width * 0.2,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(movie.posterPath)
+            ),
+          ),
+
+          // Description
+
+          const SizedBox( width: 10,),
+
+          SizedBox(
+            width: size.width * 0.7 ,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(movie.title , style: textStyles.titleMedium,),
+
+                movie.overview.length > 100 
+                ? Text('${movie.overview.substring(0 , 100)}...',)
+                : Text(movie.overview),
+
+                Row(
+                  children: [
+                    Icon(Icons.star_half_rounded , color: Colors.yellow.shade800,),
+
+                    Text(
+                      HumanFormats.humanReadbledNumber(movie.voteAverage , 1),
+
+                      style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900),
+
+                    )
+                  ],
+                )
+
+
+              ],
+            ),
+          )  
+
+        ],
+      ),
     );
   }
 }
