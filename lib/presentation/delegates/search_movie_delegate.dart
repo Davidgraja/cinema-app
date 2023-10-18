96e2b62274341aa954cf8ca12cf658d8841a1448
David Paredes <juliograjalesparedes127@gmail.com>
2023-10-18 20:03:02 +0200

    inicio de implementación de  un Bebounce para la petición de busqueda de las  movies

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domian/entities/movie.dart';
@@ -8,7 +10,34 @@ typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
   final SearchMoviesCallback searchMovies;
 
+  /// ? Implementacion de un Debounce time
+  /// Para tener una idea de lo que se trata un Debounce , basicamente se trata de la duración o periodo de tiempo  que
+  /// debe de pasar sin que un observable fuente emita  ningun valor y de esta manera despues de determinado tiempo emitir
+  /// el valor mas reciente de dicho observable
+  /// Esto es necesario cuando existe la posibilidad de que se realicen múltiples llamadas a un método con una corta duración entre si
+  /// y es deseabable que solo la última de esas llamadas invoque realmente al método de destino
+
+  // ? implementacion del debounce por medio de un Stream
+  StreamController<List<Movie>> debouncedMovies = StreamController.broadcast();
+
+  //? Un Timer Permite determinar un periodo de tiempo , al igual que limpiarlo y cancelarlo en el caso de se reciban muchos valores
+  Timer? _debounceTimer;
+
   SearchMovieDelegate({required this.searchMovies});
+
+  //? Función encargada que emite el nuevo resultado de las peliculas , se va a llamar cada vez que se añada o elimine una letra al query
+  void _onQueryChange(String query) {
+
+    print('Query String cambio');
+
+    if (_debounceTimer?.isActive ?? false) _debounceTimer!.cancel(); //? esta condicion cancela y limpia el Timer cada vez que el query cambie
+
+    _debounceTimer = Timer(const Duration(milliseconds: 500), () {
+      print('buscando peliculas');  //? Esto solo se ejecutara cuando se deja de escribir o que  el query no cambie en  500 milisegundos
+    
+      // Todo : Bucar peliculas y emitir al Stream
+    });
+  }
 
   @override
   String get searchFieldLabel => 'Buscar pelicula';
@@ -39,14 +68,21 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return FutureBuilder(
-      future: searchMovies(query),
+    //? Esta funcion se va a llamar cada vez que se añada o elimine una letra al query
+    _onQueryChange(query);
+
+    //?  uso del stream
+    return StreamBuilder(
+      stream: debouncedMovies
+          .stream, // ? El flujo que este controlador está controlando.
       builder: (context, snapshot) {
         final movies = snapshot.data ?? [];
         return ListView.builder(
             itemCount: movies.length,
-            itemBuilder: (context, index) =>
-                _MovieItemSearch(movie: movies[index]));
+            itemBuilder: (context, index) => _MovieItemSearch(
+                  movie: movies[index],
+                  onMovieSelected: close,
+                ));
       },
     );
   }
@@ -54,62 +90,69 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
 class _MovieItemSearch extends StatelessWidget {
   final Movie movie;
-  const _MovieItemSearch({required this.movie});
+  final Function onMovieSelected;
+  const _MovieItemSearch({required this.movie, required this.onMovieSelected});
 
   @override
   Widget build(BuildContext context) {
     final textStyles = Theme.of(context).textTheme;
     final size = MediaQuery.of(context).size;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10 , vertical: 5),
-      child:  Row(
-        children: [
+    return GestureDetector(
+      onTap: () {
+        onMovieSelected(context, movie);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          children: [
+            // Image
 
-         // Image
-
-          SizedBox(
-            width: size.width * 0.2,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.network(movie.posterPath)
+            SizedBox(
+              width: size.width * 0.2,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(movie.posterPath)),
             ),
-          ),
 
-          // Description
+            // Description
 
-          const SizedBox( width: 10,),
-
-          SizedBox(
-            width: size.width * 0.7 ,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(movie.title , style: textStyles.titleMedium,),
-
-                movie.overview.length > 100 
-                ? Text('${movie.overview.substring(0 , 100)}...',)
-                : Text(movie.overview),
-
-                Row(
-                  children: [
-                    Icon(Icons.star_half_rounded , color: Colors.yellow.shade800,),
-
-                    Text(
-                      HumanFormats.humanReadbledNumber(movie.voteAverage , 1),
-
-                      style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900),
-
-                    )
-                  ],
-                )
-
-
-              ],
+            const SizedBox(
+              width: 10,
             ),
-          )  
 
-        ],
+            SizedBox(
+              width: size.width * 0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    movie.title,
+                    style: textStyles.titleMedium,
+                  ),
+                  movie.overview.length > 100
+                      ? Text(
+                          '${movie.overview.substring(0, 100)}...',
+                        )
+                      : Text(movie.overview),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star_half_rounded,
+                        color: Colors.yellow.shade800,
+                      ),
+                      Text(
+                        HumanFormats.humanReadbledNumber(movie.voteAverage, 1),
+                        style: textStyles.bodyMedium!
+                            .copyWith(color: Colors.yellow.shade900),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
