6c4701ac428fa347ca2e8cd3451e9ddd02c9c9fb
David Paredes <juliograjalesparedes127@gmail.com>
2023-10-19 19:14:21 +0200

    provider para el manejo del query de busqueda añadido

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -5,3 +5,4 @@ export 'movies/movie_info_provider.dart';
 export 'movies/movie_repository_provider.dart';
 export 'movies/movies_providers.dart';
 export 'movies/movies_slideshow_provider.dart';
+export 'searchs/search_movies_provider.dart';
