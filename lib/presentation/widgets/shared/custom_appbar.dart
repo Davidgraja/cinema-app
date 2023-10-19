1a93f71df978d675bfc557b50d5b3c8273aa8f8b
David Paredes <juliograjalesparedes127@gmail.com>
2023-10-19 19:15:57 +0200

    funciones añadidas que implementan la actualizacion del query por medio de searchQueryProvider

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -38,11 +38,19 @@ class CustomAppBar extends ConsumerWidget {
                 IconButton(
                     onPressed: () {
                       final movieRepository = ref.read(movieRepositoryProvider);
+                      final searchQuery = ref.read(searchQueryProvider);
 
                       showSearch<Movie?>(
-                              context: context,
-                              delegate: SearchMovieDelegate(
-                                  searchMovies: movieRepository.searchMovies))
+                        query: searchQuery,
+                        context: context,
+                        delegate: SearchMovieDelegate(
+                          clearQuery: () => ref.read(searchQueryProvider.notifier).update((state) => '') ,
+                          searchMovies:(query){
+                            ref.read(searchQueryProvider.notifier).update((state) => query);
+                            return movieRepository.searchMovies(query);
+                          } 
+                        )
+                      )
                       .then((movie) {
                         if (movie == null) return;
                         context.push('/movie/${movie.id}');
