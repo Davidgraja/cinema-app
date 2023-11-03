8da2b490f73578e6df1917c9990bd3ecaf06aa59
David Paredes <juliograjalesparedes127@gmail.com>
2023-11-03 16:32:54 +0100

    mejora del color en el SliverAppBar

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -3,6 +3,7 @@ import 'package:cinemapedia/domian/entities/movie.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie_screen';
@@ -217,7 +218,9 @@ class _CustomSliverAppBar extends ConsumerWidget {
     return SliverAppBar(
       backgroundColor: Colors.black,
       expandedHeight: size.height * 0.7,
-      foregroundColor: Colors.white, 
+      leading: IconButton( 
+        onPressed: () => context.pop() , 
+        icon: const Icon(Icons.arrow_back_ios , color: Colors.white,) ) ,
       actions: [
         IconButton(
           onPressed: () async {
@@ -228,13 +231,13 @@ class _CustomSliverAppBar extends ConsumerWidget {
             loading: () => const CircularProgressIndicator(strokeWidth: 2,),
             data: (isFavorite) =>  isFavorite
             ? const Icon(Icons.favorite_rounded , color: Colors.red,)
-            : const Icon(Icons.favorite),
+            : const Icon(Icons.favorite , color: Colors.white,),
             error: (_, __) => throw  UnimplementedError(),
           )
         )
       ],     
-      flexibleSpace: FlexibleSpaceBar(
-        titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      flexibleSpace: FlexibleSpaceBar( 
+        titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),        
         background: Stack(
           children: [
             SizedBox.expand(
