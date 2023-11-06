8b492c98c2d8dc9190e2c9d37f1b51239ad57682
David Paredes <juliograjalesparedes127@gmail.com>
2023-11-06 19:19:32 +0100

    PopularView creado e implementado

---
+++ lib/presentation/views/movies/popular_view.dart
@@ -0,0 +1,44 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PopularView extends ConsumerStatefulWidget {
+  const PopularView({super.key});
+
+  @override
+  PopularViewState createState() => PopularViewState();
+}
+
+class PopularViewState extends ConsumerState<PopularView> {
+
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadNextPage();
+  }
+
+
+  void loadNextPage() async {
+    if(isLoading) return;
+    isLoading = true;
+    
+    await ref.read(popularMoviesProvider.notifier).loadNextPage();
+    
+    isLoading = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final popularMovies = ref.watch(popularMoviesProvider);
+
+    return Scaffold(
+
+      body:  MovieMasonry(movies: popularMovies, loadNextPage: loadNextPage ,)
+    );
+  }
+}
