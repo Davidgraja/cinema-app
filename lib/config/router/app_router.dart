36809d3730c766e641405ca125d6f049496fe5ef
David Paredes <juliograjalesparedes127@gmail.com>
2023-12-07 21:39:24 +0100

    añadida animación al navegar

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,31 +1,32 @@
-import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
+import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:cinemapedia/presentation/widgets/animations/navigation_animation.dart';
 
 final appRouter = GoRouter(initialLocation: '/home/0', routes: [
   GoRoute(
-    path: '/home/:page',
-    name: HomeScreen.name,
-    builder: (context, state) {
-      int  pageIndex = int.parse (state.pathParameters['page'] ?? '0');  
-      if(pageIndex > 2 || pageIndex < 0) {
-        pageIndex = 0;
-      }
-      return  HomeScreen(pageIndex: pageIndex);
-    },
-    routes: [
-      GoRoute(
-        path: 'movie/:id',
-        name: MovieScreen.name,
-        builder: (context, state) {
-          final movieId = state.pathParameters['id'] ?? 'no-id';
-
-          return  MovieScreen(movieId: movieId);
+      path: '/home/:page',
+      name: HomeScreen.name,
+      pageBuilder: (context, state) {
+        int  pageIndex = int.parse (state.pathParameters['page'] ?? '0');
+        if(pageIndex > 2 || pageIndex < 0) {
+          pageIndex = 0;
         }
-      ),
+        return  navigationAnimation(HomeScreen(pageIndex: pageIndex));
 
-    ]
+      },
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          pageBuilder: (context, state) {
+            final movieId = state.pathParameters['id'] ?? 'no-id';
+            return  navigationAnimation( MovieScreen(movieId: movieId));
+          },
+        ),
+
+      ]
     ),
-  
+
   GoRoute(path: '/' ,redirect: ( _ , __) => '/home/0' ,)
-  
+
   ]);
