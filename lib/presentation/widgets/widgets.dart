f6c1ab22a14ae5dd580c00bd2fc014b64531bfaa
David Paredes <juliograjalesparedes127@gmail.com>
2024-02-07 17:08:34 +0100

    archivo barril actualizado

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -7,5 +7,4 @@ export 'shared/custom_appbar.dart';
 export 'shared/custom_bottom_navigationbar.dart';
 export 'shared/full_screen_loader.dart';
 export 'animations/navigation_animation.dart';
-export 'video/movie_video.dart'; // todo : eliminar
-export 'video/movie_video_test.dart'; //todo : renombrar
+export 'video/movie_video.dart';
